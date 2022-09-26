59785d817cbf4f35b7910057c881927c872f18fb
DrewSosa <andrewsosanya@gmail.com>
2022-09-25 23:29:52 -0400

    Just a bit of commenting. Nothing crazy. Onwards for tomorrow.

--- test2.js
+++ test2.js
@@ -1,7 +1,5 @@
 console.log("Hello World!");
-console.log("i don't know why I was afraid of this javascript shit. It looked mad complicated.")
 
-console.log("Less complicated now, given that I'm tryna cop a bag.")
 // document.write("lets see if this shit works.")
 
 console.log(Math.sqrt(220));
