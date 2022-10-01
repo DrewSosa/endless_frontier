14400bef7ddd4e1f09aee8be528d147d5ee610c3
DrewSosa <andrewsosanya@gmail.com>
2022-10-01 15:55:13 -0400

    archiving old files.

--- helloworld.js
+++ helloworld.js
@@ -2,28 +2,17 @@
 
 // <script src="https://d3js.org/d3.v7.min.js"></script>
 
-console.log("Hello World!");
-console.log("i don't know why I was afraid of this javascript shit. It looked mad complicated.")
 
-console.log("Less complicated now, given that I'm tryna cop a bag.")
-// document.write("lets see if this shit works.")
-
-console.log(Math.sqrt(220));
+console.log("hello, Marble Labs speaking!")
 
 let num = 5;
 var j;  // var used in the older versions of JS
     // var is also function scoped.
 let u;  // new way to declare variables in ES6.
         // let is block scoped...recommended that we used let.
-
 let x = 5, y =6, z = 7
-
-
 let car = {type: "Fiat", model: "500", color: "white" };
-
 console.log(car); console.log(car.type)
-console.info()
-
 // const number = prompt('Enter the number: ');
 // const result = Math.sqrt(number);
 // console.log(`The square root of ${number} is ${result}`);
--- test2.js
+++
@@ -1,7 +0,0 @@
-console.log("Hello World!");
-
-// document.write("lets see if this shit works.")
-
-console.log(Math.sqrt(220));
-
-let num = 5;
